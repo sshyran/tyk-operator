fdd3c1f992529510242f2127dd36638f4fc5afc3
Ahmet Soormally <ahmet@mangomm.co.uk>
2022-01-20 15:00:19 +0000

    test

---
+++ api/v2alpha1/apidefinition_types.go
@@ -0,0 +1,64 @@
+/*
+
+
+Licensed under the Mozilla Public License (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.mozilla.org/en-US/MPL/2.0/
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v2alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ApiDefinitionSpec defines the desired state of ApiDefinition
+type ApiDefinitionSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of ApiDefinition. Edit apidefinition_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// ApiDefinitionStatus defines the observed state of ApiDefinition
+type ApiDefinitionStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// ApiDefinition is the Schema for the apidefinitions API
+type ApiDefinition struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ApiDefinitionSpec   `json:"spec,omitempty"`
+	Status ApiDefinitionStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ApiDefinitionList contains a list of ApiDefinition
+type ApiDefinitionList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ApiDefinition `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ApiDefinition{}, &ApiDefinitionList{})
+}
